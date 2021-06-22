b5a97f55367c0482107e210afc115c9df3ef098a
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-06-21 19:39:26 -0500

    craecion de UsuarioController, UsuarioRepository y entidad Usuario.java

---
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping(value = "/usuario",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity listarProductos(){
+        return new ResponseEntity(usuarioRepository.findAll(), HttpStatus.OK);
+    }
+}
+
---
+++ src/main/java/com/example/demo/entity/Usuario.java
@@ -0,0 +1,47 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    private String correo;
+    private int idarea;
+    private String nombres;
+    private String apellidos;
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
