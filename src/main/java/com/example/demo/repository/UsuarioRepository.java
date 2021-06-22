49e038da35dae1e3752423747346ee3fa75cb984
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-06-21 20:20:02 -0500

    Flujo Usuario

--- src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -4,7 +4,12 @@ import com.example.demo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByCorreoEquals(String correo);
+    Boolean existsByCorreoEquals(String correo);
+
 }
 
